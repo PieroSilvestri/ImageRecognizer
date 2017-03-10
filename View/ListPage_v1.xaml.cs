47276866000854496082d64391fd6753932beb08
munsra <piero.silvestri93@gmail.com>
2017-03-10 16:36:13 +0100

    Progetto di base

---
+++ View/ListPage_v1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImageRecognizer
+{
+	public partial class ListPage_v1 : ContentPage
+	{
+		public ListPage_v1()
+		{
+			InitializeComponent();
+		}
+	}
+}
