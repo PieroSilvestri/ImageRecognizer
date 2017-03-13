4e1692fb9cef03b0e70c3cfda96e3610be6ac87e
munsra <piero.silvestri93@gmail.com>
2017-03-13 09:27:20 +0100

    Vedemo dai

---
+++ View/ProvaPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImageRecognizer
+{
+	public partial class ProvaPage : ContentPage
+	{
+		public ProvaPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
