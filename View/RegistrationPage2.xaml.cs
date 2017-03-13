38cbbc1ebd291024d4651a462c5bd938d7a39114
Unknown <scocchiello@gmail.com>
2017-03-13 09:16:41 +0100

    E fin qui tutto bene?

---
+++ View/RegistrationPage2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImageRecognizer
+{
+	public partial class RegistrationPage2 : ContentPage
+	{
+		public RegistrationPage2()
+		{
+			InitializeComponent();
+		}
+	}
+}
