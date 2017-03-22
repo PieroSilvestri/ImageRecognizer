a9455021c85ee303945f22d41dc27a5f6fe0ea62
munsra <piero.silvestri93@gmail.com>
2017-03-22 11:41:34 +0100

    Puttanate

---
+++ View/ImageEmotionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImageRecognizer
+{
+	public partial class ImageEmotionPage : ContentPage
+	{
+		public ImageEmotionPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
