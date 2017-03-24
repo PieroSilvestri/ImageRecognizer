8266f7fa76f6bba4dc1d1328e12205d38d9f13da
Unknown <scocchiello@gmail.com>
2017-03-24 17:44:15 +0100

    PUSHONE REPORT

--- View/ProvaPage.xaml.cs
+++ View/ProvaPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ImageRecognizer
@@ -10,9 +10,17 @@ namespace ImageRecognizer
 		public ProvaPage()
 		{
 			InitializeComponent();
+			this.Title = "Report";
+			progressBar();
+
 
 		}
 
+		async void progressBar() 
+		{ 
+			await Task.Delay(2000);
+			await barHappy.ProgressTo(.8, 1000, Easing.Linear);
+		}
 
 		public async void DoLoginWithPassword(object o, EventArgs e)
 		{
--- View/ReportPage.xaml.cs
+++ View/ReportPage.xaml.cs
@@ -11,10 +11,20 @@ namespace ImageRecognizer
 
 		private int user_id;
 		MainViewModel vm;
+		double angerValue;
+		double contemptValue;
+		double disgustValue;
+		double fearValue;
+		double happinessValue;
+		double neutralValue;
+		double sadnessValue;
+		double surpriseValue;
+
+
 
 		public ReportPage(int id)
 		{
-			//InitializeComponent();
+			InitializeComponent();
 			vm = new MainViewModel();
 			Title = "Report";
 			this.user_id = id;
@@ -31,9 +41,40 @@ namespace ImageRecognizer
 				Debug.WriteLine("GetEmotionsReport");
 				Debug.WriteLine(myReport);
 
-				ReportItem reportItem = new ReportItem();
-				reportItem.Success = (bool)myReport["success"];
-				reportItem.Average_age = (int)myReport["average_age"];
+				maleCounter.Text = myReport["male_count"].ToString();
+				femaleCounter.Text = myReport["female_count"].ToString();
+				childCounter.Text = myReport["not_adult_count"].ToString();
+				maleOldest.Text = myReport["male_older_age"].ToString();
+				femaleOldest.Text = myReport["female_older_age"].ToString();
+				maleYoungest.Text = myReport["male_younger_age"].ToString();
+				femaleYoungest.Text = myReport["female_younger_age"].ToString();
+
+				ageAverage.Text = "Average: " + (myReport["average_age"]).ToString();
+				int tot = (int)myReport["total_face_detected"];
+
+				angerValue = ((int)myReport["anger_count"])/tot;
+				contemptValue = ((int)myReport["contempt_count"]) / tot;
+				disgustValue = ((int)myReport["disgust_count"]) / tot;
+				fearValue = ((int)myReport["fear_count"]) / tot;
+				happinessValue = ((int)myReport["happiness_count"]) / tot;
+				neutralValue = ((int)myReport["neutral_count"]) / tot;
+				sadnessValue = ((int)myReport["sadness_count"]) / tot;
+				surpriseValue = ((int)myReport["surprise_count"]) / tot;
+
+				/*
+				 *   "total_face_detected": 4,
+					  "anger_count": 0,
+					  "contempt_count": 0,
+					  "disgust_count": 1,
+					  "fear_count": 0,
+					  "happiness_count": 1,
+					  "neutral_count": 2,
+					  "sadness_count": 0,
+					  "surprise_count": 0,
+					  "top_emotion": "neutral"
+				 * 
+				 * 
+				 * reportItem.Success = (bool)myReport["success"];
 				reportItem.Male_count = (int)myReport["male_count"];
 				reportItem.Female_count = (int)myReport["female_count"];
 				reportItem.Not_adult_count = (int)myReport["not_adult_count"];
@@ -43,9 +84,9 @@ namespace ImageRecognizer
 				reportItem.Number_of_face_detected_today = (int)myReport["number_of_face_detected_today"];
 				reportItem.Total_face_detected = (int)myReport["total_face_detected"];
 				reportItem.Description = myReport["description"].ToString();
+*/
 
-				InitializeComponent();
-				this.BindingContext = reportItem;
+				//this.BindingContext = reportItem;
 			}
 			catch (Exception exc)
 			{
@@ -57,20 +98,28 @@ namespace ImageRecognizer
 
 
 		}
-		/*
-		  "success": "true",
-		  "average_age": 23,
-		  "male_count": 5,
-		  "female_count": 0,
-		  "not_adult_count": 0,
-		  "male_older_age": 32,
-		  "female_older_age": 0,
-		  "male_younger_age": 21,
-		  "female_younger_age": 0,
-		  "last_face_detected": "3/22/2017 4:45:36 PM",
-		  "number_of_face_detected_today": 5,
-		  "total_face_detected": 5,
-		  "description": "Data Analysis of ID: 18"
-		*/
+
+		async void calculateEmotions(object o, EventArgs e)
+		{ 
+			await barHappy.ProgressTo(.7, 1000, Easing.Linear);
+			await barFear.ProgressTo(.5, 1000, Easing.Linear);
+			await barAnger.ProgressTo(.3, 1000, Easing.Linear);
+			await barContempt.ProgressTo(contemptValue, 1000, Easing.Linear);
+			await barDisgust.ProgressTo(disgustValue, 1000, Easing.Linear);
+			await barSadness.ProgressTo(sadnessValue, 1000, Easing.Linear);
+			await barSurprised.ProgressTo(surpriseValue, 1000, Easing.Linear);
+			await barNeutral.ProgressTo(neutralValue, 1000, Easing.Linear);
+		}
 	}
 }
+/*
+"total_face_detected": 4,
+					  "anger_count": 0,
+					  "contempt_count": 0,
+					  "disgust_count": 1,
+					  "fear_count": 0,
+					  "happiness_count": 1,
+					  "neutral_count": 2,
+					  "sadness_count": 0,
+					  "surprise_count": 0,
+*/
